1b144b8a833cb296550e488bc7a0adc67e2058e7
arigatomoki <aritomo@arigasatoshitakanoMacBook-Pro.local>
2023-03-17 01:34:04 +0900

    切り替え/Test用のRepository作成

---
+++ CustomHUDs/CustomHUDsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHUDsApp.swift
+//  CustomHUDs
+//
+//  Created by 有賀智貴 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct CustomHUDsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
